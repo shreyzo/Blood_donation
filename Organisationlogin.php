d94505becf6f75faedd40cb23e344e0ee67419b7
shreyzo <zodeshreya@gmail.com>
2019-04-16 22:13:15 +0530

    new

---
+++ Organisationlogin.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$conn=mysqli_connect("localhost","root","","blooddonation");
+
+if(!$conn)
+{
+  die("connection failed".mysqli_connect_error());
+  echo "fail";
+}
+
+$Username = $_POST['username'];
+$pwd = $_POST['password'];
+$_SESSION['Username'] = $Username;
+
+$sql= "SELECT * FROM organisationtable WHERE DIN='$Username' AND Password= '$pwd'";
+
+$result = $conn->query($sql);
+if (!$row = $result->fetch_assoc()) {
+   echo "Username or password incorrect!";
+}
+else {
+  echo "Logged in";
+	//starting session
+	//session_start();
+	//$_SESSION["username"]=
+  header("Location: organisationdisplay.php");
+
+}
+
+
+?>
