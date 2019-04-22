0a741d9a7816fc44817ea4e9736d04690b4d9b1a
Apurvamahajan24 <apurvamahajan246@gmail.com>
2019-04-22 11:57:29 +0530

    Minor

--- organisationdisplay.php
+++ organisationdisplay.php
@@ -2,9 +2,15 @@
 <html>
 <head>
   <title>IMAGE UPLOAD</title>
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+      <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="static/css/materialize.min.css"  media="screen,projection"/>
+  <link rel="stylesheet" type="text/css" href="static/prelogin.css">
 
 </head>
 </body>
+<div class="a1">
   <div id="content">
     <form method="post" action=organisationdisplay1.php enctype="multipart/form-data">
       <input type ="hidden" name="size" value="10000000">
@@ -21,6 +27,7 @@
 
     </form>
   </div>
+</div>
 </head>
 </html>
 <?php
